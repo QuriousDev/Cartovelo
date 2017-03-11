aa33e9106e833f22bcac35daa70a8ad04774d508
Philippe Girard <philippe.girard5@gmail.com>
2017-03-11 10:15:04 -0500

    create s4 code

--- controllers/paths.js
+++ controllers/paths.js
@@ -1,33 +1,11 @@
-var issues = require('../models/paths')
-var AWS = require('aws-sdk');
-AWS.config.update({
-    signatureVersion: 'v4'
-});
-var s3 = new AWS.S3();
-// Les noms de compartiments doivent être uniques pour tous les utilisateurs S3
-var myBucket = 'test-03de30f2-15e8-4929-98d5-9cf8275313fc';
-var myKey = 'HelloWorld.txt';
+var aws = require('../helpers/s3.js')
+
 
 module.exports = {
 
   paths: function(req, res, next) {
-      // create file in bucket and make it public-read
-      s3.createBucket({Bucket: myBucket}, function(err, data) {
-      if (err) {
-         console.log(err);
-         } else {
-           params = {Bucket: myBucket, Key: myKey, Body: 'Hello 2!', ACL: 'public-read'};
-           s3.putObject(params, function(err, data) {
-               if (err) {
-                   console.log(err)
-               } else {
-                   console.log("Successfully uploaded data to myBucket/myKey");
-               }
-            });
-         }
-      });
-
-      res.send('File ' + myBucket + ' ' + myKey + ' should be created in s3');
+      aws.saveToImageBucket('testfile.txt', 'hello world')
+      res.send('test be created in s3');
   },
 
   city_activity: function(req, res, next) {
@@ -35,16 +13,9 @@ module.exports = {
     activity = req.params.activity
     if(city == "sherbrooke" && activity == "bike") {
 
-      var params = {Bucket: "hackqc-roads", Key: "sherbrooke_pistes_cyclables.json"};
-      s3.getObject(params, function(err, json_data) {
-        if (!err) {
-          var json = JSON.parse(new Buffer(json_data.Body).toString("utf8"));
-         res.send(json)
-       } else {
-        res.send("There was an error : " + err)
-       }
-      });
-
+      aws.getFromRoadBucket("sherbrooke_pistes_cyclables.json", function(s3reponse) {
+        res.send(s3reponse)
+      })
     }
   }
 };
